83bb794d5ecf8b03701431c5a3b7f8ec66ce0a0e
54Alexzen <alexzen.develop@gmail.com>
2025-09-27 00:50:36 -0600

    feat(footer): Add Footer component with creator attribution

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-stone-50 flex justify-center items-center border-t border-t-stone-200 py-4">
+      <p className="text-center sm:text-xs text-2xs">
+        Creado por <span className="font-bold">Ulises Jimenez Cruz</span>
+      </p>
+    </footer>
+  );
+}
