2f16edfd984ff615db7973c04dbbdeb769dc43ae
54Alexzen <alexzen.develop@gmail.com>
2025-09-27 15:49:46 -0600

    fix(layout): Añadir propiedad richColors al componente Toaster para mejorar la visualización

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <AuthProvider>
           <Navbar />
           {children}
-          <Toaster position="top-center" />
+          <Toaster position="top-center" richColors/>
         </AuthProvider>
         <Footer />
       </body>
