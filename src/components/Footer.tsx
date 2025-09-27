33c9db60841bbc8a6c84231e6e5b6a4e9dc51dd9
54Alexzen <alexzen.develop@gmail.com>
2025-09-27 15:15:19 -0600

    fix(footer): Remove background and border styles from Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-stone-50 flex justify-center items-center border-t border-t-stone-200 py-4">
+    <footer className=" flex justify-center items-center py-4">
       <p className="text-center sm:text-xs text-2xs">
         Creado por <span className="font-bold">Ulises Jimenez Cruz</span>
       </p>
