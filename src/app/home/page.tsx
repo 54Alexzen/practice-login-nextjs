d120b46b102f46e77e4c5df0748c286a713cf634
54Alexzen <alexzen.develop@gmail.com>
2025-09-26 22:26:20 -0600

    fix(home): Add session check and redirect to login if not authenticated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,22 @@
+import LogoutButton from "@/components/LogoutButton";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
   return (
     <main>
       <section>
         <h1>Home Page</h1>
         <p>{session?.user?.email}</p>
       </section>
+
+      <LogoutButton />
     </main>
   );
 }
