9828f172dce5ed88739e922e890bc7048c779348
54Alexzen <alexzen.develop@gmail.com>
2025-09-27 15:33:29 -0600

    feat(navbar): Reemplazar enlaces de texto por iconos de Facebook e Instagram en el componente Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,44 @@
+import { FacebookIcon } from "./icons/FacebookIcon";
+import { InstagramIcon } from "./icons/InstagramIcon";
+
 const socialLinks = [
   {
     name: "Facebook",
     url: "https://facebook.com",
-  },
-  {
-    name: "Twitter",
-    url: "https://twitter.com",
+    icon: <FacebookIcon className="md:size-5 sm:size-4 size-3.5" />,
   },
   {
     name: "Instagram",
     url: "https://instagram.com",
+    icon: <InstagramIcon className="md:size-5 sm:size-4 size-3.5" />,
   },
 ];
 
 export default function Navbar() {
   return (
-    <header className="fixed bg-white/10 backdrop-blur-md w-full top-0 left-0 z-20 border-b border-b-stone-200 min-h-[5svh] flex items-center px-4">
-      <nav>
+    <header className="fixed w-full top-0 left-0 z-20 border-b border-b-stone-200 bg-stone-50/50 backdrop-blur-sm">
+      <nav className="container mx-auto flex justify-between items-center py-2.5 md:px-6 sm:px-5 px-4">
         <div>
-            <p>Ulises Jimenez Cruz</p>
+          <p className="md:text-sm sm:text-xs text-2xs font-semibold">
+            Ulises Jimenez Cruz
+          </p>
+          <p className="md:text-xs text-2xs text-stone-500">
+            Desarrollador Frontend
+          </p>
         </div>
-        <div>
-            {socialLinks.map((link) => (
-          <a
-            key={link.name}
-            href={link.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-2 text-white hover:underline"
-          >
-            {link.name}
-          </a>
-        ))}
+        <div className="flex items-center gap-1.5">
+          {socialLinks.map((link) => (
+            <a
+              key={link.name}
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="border border-stone-800 hover:bg-stone-800 hover:text-white transition-colors duration-300 rounded-full p-1"
+              aria-label={`Visitar ${link.name}`}
+            >
+              {link.icon}
+            </a>
+          ))}
         </div>
       </nav>
     </header>
