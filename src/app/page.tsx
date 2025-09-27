b584c0b3747bf4bb121ddd3fd3cba11f4b76bc53
54Alexzen <alexzen.develop@gmail.com>
2025-09-27 15:33:42 -0600

    feat(login): Mejorar estilos y responsividad en el formulario de inicio de sesión

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,10 +45,13 @@ export default function Login() {
   return (
     <main className="flex justify-center items-center min-h-[90svh] w-full">
       <div className="absolute inset-0 -z-10 background" />
-      <section className="container mx-auto flex flex-col justify-center items-center">
-        <h1 className="sm:text-4xl text-3xl font-bold uppercase">Bienvenido</h1>
-        <p className="max-w-md text-center sm:text-sm text-xs text-stone-500 my-3">
-          Si ya cuentas con una cuenta, inicia sesión. De lo contrario, regístrate en la parte inferior.
+      <section className="container mx-auto flex flex-col justify-center items-center md:px-6 sm:px-5 px-4">
+        <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold uppercase">
+          Bienvenido
+        </h1>
+        <p className="max-w-md text-center md:text-sm sm:text-xs text-2xs text-stone-500 my-3">
+          Si ya cuentas con una cuenta, inicia sesión. De lo contrario,
+          regístrate en la parte inferior.
         </p>
         <form
           onSubmit={handleSubmit(onSubmit)}
@@ -73,28 +76,30 @@ export default function Login() {
           <button
             type="submit"
             disabled={!isValid || isSubmitting}
-            className="w-full bg-stone-800 text-white rounded-full sm:py-2.5 py-2 mt-10 uppercase font-semibold sm:text-sm text-xs flex justify-center items-center gap-2 disabled:cursor-not-allowed disabled:opacity-50"
+            className="w-full bg-stone-800 text-white rounded-full py-2.5 mt-10 uppercase font-semibold md:text-sm sm:text-xs text-2xs flex justify-center items-center gap-2 disabled:cursor-not-allowed disabled:opacity-50"
           >
             {isSubmitting ? (
               <>
-                <Loader className="animate-spin size-4" />
+                <Loader className="animate-spin md:size-4 size-3.5" />
                 <span>Iniciando sesión</span>
               </>
             ) : (
               <>
-                <Send className="size-4" />
+                <Send className="md:size-4 size-3.5" />
                 <span>Iniciar Sesión</span>
               </>
             )}
           </button>
         </form>
+        
         <div>
-          <div className="flex items-center my-4">
+          <div className="flex items-center my-4 md:text-base sm:text-sm text-xs">
             <div className="flex-grow border-t border-gray-300"></div>
-            <span className="mx-4 text-stone-500">o</span>
+            <span className="mx-4 text-stone-500">O</span>
             <div className="flex-grow border-t border-gray-300"></div>
           </div>
-          <div className="flex justify-center items-center gap-2 sm:text-sm text-xs">
+          
+          <div className="flex justify-center items-center gap-2 md:text-sm sm:text-xs text-2xs">
             <p>¿No tienes una cuenta?</p>
             <Link href="/register" className="underline font-semibold">
               Regístrate
