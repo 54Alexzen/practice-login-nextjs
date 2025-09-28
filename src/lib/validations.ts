d464863a0bbf8d8cde80b3b48e2d07ed56be8b85
54Alexzen <alexzen.develop@gmail.com>
2025-09-27 22:11:39 -0600

    fix(validations): Ajustar las validaciones de contraseña y nombre en los esquemas de inicio de sesión y registro

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -2,16 +2,22 @@ import z from "zod";
 
 export const loginSchema = z.object({
   email: z.email("Dirección de correo no válida"),
-  password: z.string().min(6).max(100).nonempty("La contraseña es obligatoria"),
+  password: z
+    .string()
+    .min(6, "La contraseña debe tener al menos 6 caracteres")
+    .max(50, "La contraseña no puede exceder los 50 caracteres"),
 });
 
 export const registerSchema = z.object({
   name: z
     .string()
     .min(2, "El nombre debe tener al menos 2 caracteres")
-    .max(100),
+    .max(100, "El nombre no puede exceder los 100 caracteres"),
   email: z.email("Dirección de correo no válida"),
-  password: z.string().min(6).max(100).nonempty("La contraseña es obligatoria"),
+  password: z
+    .string()
+    .min(6, "La contraseña debe tener al menos 6 caracteres")
+    .max(50, "La contraseña no puede exceder los 50 caracteres"),
 });
 export type LoginSchemaType = z.infer<typeof loginSchema>;
 export type RegisterSchemaType = z.infer<typeof registerSchema>;
