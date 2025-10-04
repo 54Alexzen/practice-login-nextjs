27ebdb61c14f7a4e7a36f9da108134b525cf43cb
54Alexzen <alexzen.develop@gmail.com>
2025-10-04 16:52:06 -0600

    feat: Remove default href value in Button component for better flexibility

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -16,7 +16,7 @@ export const Button: React.FC<ButtonProps> = ({
   disabled,
   variant = "default",
   className = "",
-  href = "#",
+  href,
   pathname,
 }: ButtonProps) => {
   const isActive = pathname === href;
